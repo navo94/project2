5ca35cd1cc66457e85d5e3ff1ec70613bddd82dc
Navonil Nath <navonilai@gmail.com>
2025-08-09 17:17:09 +0530

    server.js modified

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -1,9 +1,31 @@
 //creating a basic server
-const http=require('http');
+/**const http=require('http');
 const server=http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'plain/text'});
     res.end("hello world");
 });
 server.listen(3001,()=>{
     console.log("running the http://localhost:3001");
+});**/
+
+
+//lets make the server a little more smatter
+//by enabling it to handle different reequests through the url
+//modifying thr server.js to handle the POST & /signup requests
+
+const http = require('http');
+const host='localhost';
+const port=3001;
+const server=http.createServer((req,res)=>{
+    if(req.url=='/signup' && req.method=='POST'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end("signup request received");
+    }
+    else{
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('400,error ');
+    }
+});
+server.listen(port,host,()=>{
+    console.log(`server runnong at the http://${host}:${port}`);
 });
